5c96ee3381f2b8eb7d097a353b8532f0eda342e8
brockrossman <brock.rossman@gmail.com>
2025-09-18 12:05:43 -0500

    zzzz

--- app/src/main/java/com/example/listviewexample/MainActivity.java
+++ app/src/main/java/com/example/listviewexample/MainActivity.java
@@ -3,14 +3,34 @@ package com.example.listviewexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Random;
+
+import Model.Pet;
 
 public class MainActivity extends AppCompatActivity {
 
     ListView petList;
+    ArrayList<Pet> pets;
+
+    AdapterView.OnItemClickListener selectListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            Pet selected = (Pet) parent.getItemAtPosition(position);
+            Toast.makeText(getApplicationContext(), "hi hi " + selected.getName(), Toast.LENGTH_SHORT).show();
+        }
+
+    };
 
 
     @Override
@@ -19,9 +39,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         petList = findViewById(R.id.listView);
+        pets = new ArrayList<>();
+        pets.add(new Pet("Kenny", "Husky", 17));
+        pets.add(new Pet("Arnold", "Golden Retriever", 7));
+        pets.add(new Pet("Bomber", "Malimute", 1));
+        pets.add(new Pet("killa", "Corgi", 12));
 
-        int selection = 1;
+
+
+        int selection = 2;
         populateListView(selection);
+
+
 
 
 
@@ -34,6 +63,11 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case 2:
                 usingDataArray();
+                petList.setOnItemClickListener(selectListener);
+                break;
+
+            case 3:
+                usingTwoLines();
                 break;
 
         }
@@ -45,7 +79,8 @@ public class MainActivity extends AppCompatActivity {
      */
     private void useStringResource() {
         String [] content = getResources().getStringArray(R.array.breeds);
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, content);
+        ArrayAdapter adapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, content);
         petList.setAdapter(adapter);
     }
 
@@ -54,6 +89,30 @@ public class MainActivity extends AppCompatActivity {
      */
     private void usingDataArray() {
 
+        ArrayAdapter<Pet> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, pets);
+        petList.setAdapter(adapter);
 
     }
-}
+
+    private void usingTwoLines(){
+        List<HashMap<String, String>> data = new ArrayList<>();
+
+        for(Pet P : pets) {
+            HashMap<String, String> current = new HashMap<>();
+            current.put("name", P.getName());
+            current.put("breed", P.getBreed());
+            data.add(current);
+        }
+
+        SimpleAdapter adapter = new SimpleAdapter(this,
+                data,
+                android.R.layout.simple_list_item_2, new String[]{"name", "breed"},
+                new int[]{android.R.id.text1, android.R.id.text2});
+
+        petList.setAdapter(adapter);
+
+
+
+    }
+};
